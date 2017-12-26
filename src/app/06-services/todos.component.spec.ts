a3a58e7edc50cca8f40f01b0339b19136dd7144b
pashko.ostap <pashko.ostap@gmail.com>
2017-12-26 21:58:04 +0200

    wrote tests for testing post and rejected requests

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,8 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -22,5 +24,33 @@ describe('TodosComponent', () => {
 
       expect(component.todos.length).toBe(3);
     });
+  });
+
+  it('should call the server to save the changes when a new todo item is added', () => {
+    let spy = spyOn(service, 'add').and.callFake(todo => {
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo iten returned from the server', () => {
+    let todo = { id: 1, title: 'new todo title' };
+    let spy = spyOn(service, 'add').and.returnValue(Observable.from([todo]));
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should set the message property of server return an error when adding a new todo', () => {
+    let err = 'Some error';
+    let spy = spyOn(service, 'add').and.returnValue(Observable.throw(err));
+
+    component.add();
+
+    expect(component.message).toBe(err);
   });
 });
