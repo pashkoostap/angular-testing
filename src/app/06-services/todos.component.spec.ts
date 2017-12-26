57092eb47f3519a276c6534fbcea93960d3c0d1b
pashko.ostap <pashko.ostap@gmail.com>
2017-12-26 21:47:30 +0200

    wrote test for the angular service testing

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,26 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with the items returned from the server', () => {
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([
+        [{ id: 1, title: 'a' }, { id: 2, title: 'b' }, { id: 3, title: 'c' }]
+      ]);
+
+      component.ngOnInit();
+
+      expect(component.todos.length).toBe(3);
+    });
   });
 });
